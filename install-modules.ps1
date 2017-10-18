4dfe6c5c7490793bc46a1d6497f0d714c9b6e2d6
bergmeister <c.bergmeister@gmail.com>
2017-10-18 22:01:09 +0100

    Update install-modules.ps1
    Use advanced function

--- install-modules.ps1
+++ install-modules.ps1
@@ -1,5 +1,6 @@
 # NB Execution policy needs to be set to RemoteSigned or Unrestricted for some of them using Set-ExecutionPolicy
-
+ [CmdletBinding()]Param()
+ 
 Install-Module posh-docker       -Scope CurrentUser -Force
 Install-Module posh-git          -Scope CurrentUser -Force
 Install-Module posh-with         -Scope CurrentUser -Force
