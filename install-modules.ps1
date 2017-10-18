164ab704ad29287f5fffb6e027f275fea2471eef
bergmeister <c.bergmeister@gmail.com>
2017-10-18 22:11:51 +0100

    Update install-modules.ps1
    install pester only if not already installed

--- install-modules.ps1
+++ install-modules.ps1
@@ -1,10 +1,23 @@
 # NB Execution policy needs to be set to RemoteSigned or Unrestricted for some of them using Set-ExecutionPolicy
  [CmdletBinding()]Param()
  
+Function Install-ModuleOnlyIfNotAlreadyInstalled($ModuleName)
+{
+    if (-not (Get-Module -ListAvailable $ModuleName))
+    {
+        Write-Verbose "Installing Module $($ModuleName)"
+        Install-Module $ModuleName -Scope CurrentUser -Force
+    }
+    else
+    {
+        Write-Verbose "Module $($ModuleName)" is already installed"
+    }
+}
+ 
 Install-Module posh-docker       -Scope CurrentUser -Force
 Install-Module posh-git          -Scope CurrentUser -Force
 Install-Module posh-with         -Scope CurrentUser -Force
-Install-Module Pester            -Scope CurrentUser -Force
+Install-ModuleOnlyIfNotAlreadyInstalled Pester
 Install-Module PSScriptAnalyzer  -Scope CurrentUser -Force
 Install-Module Jump-Location     -Scope CurrentUser -Force
 
