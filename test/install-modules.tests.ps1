11b13a51c16a98b1af400d49e0432a8dfbb55d8b
bergmeister <c.bergmeister@gmail.com>
2017-10-19 08:31:08 +0100

    CI Smoke tests using Pester (#5)
    * simple pester tests (pester v3)
    
    * Run pester tests in appveyor
    
    * add more test cases
    
    * produce pester output xml file and upload (from https://github.com/pester/Pester/wiki/Showing-Test-Results-in-CI-%28TeamCity%2C-AppVeyor%29)
    
    * fix appveyor syntax and add chocolatey test

---
+++ test/install-modules.tests.ps1
@@ -0,0 +1,18 @@
+Describe 'Modules are installed' {
+
+    It "Module '<moduleName>' is installed" -TestCase @(
+        @{ moduleName = "Pester";  }
+        @{ moduleName = "posh-docker";  }
+        @{ moduleName = "posh-git"; }
+        @{ moduleName = "posh-with"; }
+        @{ moduleName = "PoShFuck"; }
+        @{ moduleName = "Jump.Location"; }
+        @{ moduleName = "PSScriptAnalyzer"; } ){
+        Param ($moduleName)
+          Get-Module -ListAvailable $moduleName | Should Not Be $null
+    }
+
+    It "Chocolatey is installed" {
+      Get-Command choco | Should Not Be $null
+    }
+}
