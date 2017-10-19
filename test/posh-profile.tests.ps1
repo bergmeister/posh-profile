4800ee3b126ae651240243b806aa35d143517b61
Christoph Bergmeister <c.bergmeister@gmail.com>
2017-10-19 22:13:31 +0100

    fix syntax of posh-profile.tests.ps1

--- test/posh-profile.tests.ps1
+++ test/posh-profile.tests.ps1
@@ -1,29 +1,28 @@
 Describe 'posh-profile.psm1' {
     
     Import-Module (Join-Path (Split-Path $PSScriptRoot) 'posh-profile.psm1') -DisableNameChecking
-    
-        It "Set-MsBuildExeVariablesForEnterpriseEdition has set variables" {
-            $msBuildVS2015 | Should Not BeNullOrEmpty
-            $msBuildVS2017 | Should Not BeNullOrEmpty
-            $msBuildVS2017Preview | Should Not BeNullOrEmpty
-        }
 
-        It "'which' works with cmd" {
-            which 'cmd' | Should Contain 'cmd.exe'
-        }
+    It "Set-MsBuildExeVariablesForEnterpriseEdition has set variables" {
+        $msBuildVS2015 | Should Not BeNullOrEmpty
+        $msBuildVS2017 | Should Not BeNullOrEmpty
+        $msBuildVS2017Preview | Should Not BeNullOrEmpty
+    }
 
-        It "Save-History produces file with history" {
-            $childItem = Get-ChildItem # invoke some command
-            try
-            {
-                $tempFile = [System.IO.Path]::GetTempFileName()
-                Save-History $tempFile
-                Get-Content $tempFile -Raw | Should BeLike '*Get-ChildItem*'
-            }
-            finally
-            {
-                Remove-Item $tempFile
-            }
+    It "'which' works with cmd" {
+        which 'cmd' | Should Contain 'cmd.exe'
+    }
+
+    It "Save-History produces file with history" {
+        $childItem = Get-ChildItem # invoke some command
+        try
+        {
+            $tempFile = [System.IO.Path]::GetTempFileName()
+            Save-History $tempFile
+            Get-Content $tempFile -Raw | Should BeLike '*Get-ChildItem*'
+        }
+        finally
+        {
+            Remove-Item $tempFile
         }
     }
 }
