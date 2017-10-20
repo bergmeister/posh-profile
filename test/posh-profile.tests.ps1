25c594fa74b314a4747eb61b4bc3b79bab63bf75
bergmeister <c.bergmeister@gmail.com>
2017-10-20 20:32:44 +0100

    add more posh-profile tests (#11)
    * first try to upload code coverage report
    
    * syntax fix
    
    * fix call to codecov
    
    * code coverage badge
    
    * fix badges
    
    * fix msdbuild cmdlet
    
    * Set-LocationToCurrentIseItem test
    
    * fix set-currentlocationtoiseitem test and warnings
    
    * fix pssa warning
    
    * add neagtive test for Set-LocationToCurrentIseItem
    
    * fix msbuild command again and add test for it now
    
    * supportsshouldprocess tests
    
    * fix merge error

--- test/posh-profile.tests.ps1
+++ test/posh-profile.tests.ps1
@@ -1,3 +1,7 @@
+# Global variable needed for mocking of $psISE variable for tests of Set-LocationToCurrentIseItem
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidGlobalVars", "")]
+param()
+
 Describe 'posh-profile.psm1' {
     
     It "Can import module" {
@@ -13,5 +17,37 @@ Describe 'posh-profile.psm1' {
     It "'which' works with cmd" {
         which 'cmd' | Should Contain 'cmd.exe'
     }
+
+    It 'Set-LocationToCurrentIseItem throws if not in ISE' {
+        $global:psISE = $null
+        { Set-LocationToCurrentIseItem } | Should throw
+    }
+
+    It "Set-LocationToCurrentIseItem works if ISE environment variable is set" {
+        
+        $initialLocation = Get-Location
+        try {
+            $tempFolder = [System.IO.Path]::GetTempPath()
+            $global:psISE = @{CurrentFile=@{}}
+            $global:psISE.CurrentFile.Add('FullPath',$tempFolder)
+            Set-LocationToCurrentIseItem
+            (Get-Location).Path | Should Be (Split-Path $tempFolder)
+        }
+        finally {
+            Set-Location $initialLocation
+        }
+    }
+
+    It "Set-MsBuildExeVariablesForEnterpriseEdition supports ShouldProcess" {
+        Set-MsBuildExeVariablesForEnterpriseEdition -WhatIf
+    }
+
+    It "ShouldProcess for git helpers" {
+        Update-GitSubmoduleRemote -WhatIf
+        Update-GitSubmodule -WhatIf
+        New-Feature 'myfeatureName' -WhatIf
+        Update-BranchFromDevelop -WhatIf
+    }
+
 }
 
