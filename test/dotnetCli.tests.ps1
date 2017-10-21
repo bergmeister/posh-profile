9ac6a2eaa61cf28f2cca5a4cce1e36acd4416f87
bergmeister <c.bergmeister@gmail.com>
2017-10-21 17:27:37 +0100

    suppress red herring bug in dotnet cli in appveyor  (#14)
    * Update dotnetCli.tests.ps1
    
    * Redirect stderr to null since the 'dotnet new xunit' command works but throws an error that can be ignored

--- test/dotnetCli.tests.ps1
+++ test/dotnetCli.tests.ps1
@@ -9,7 +9,7 @@ Describe 'dotnetCli' {
             $tempFolder = New-Item -ItemType Directory -Name tempdotnetCliTestFolder
             Set-Location $tempFolder
             Get-Command dotnet | Should Not Be Null
-            dotnet new xunit # this does not work in CI unfortunately
+            dotnet new xunit  2>&1 # this throws a red herring error in Appveyor: https://github.com/appveyor/ci/issues/1873
             dotnetclean
             dotnetrestore
             dotnetbuild
